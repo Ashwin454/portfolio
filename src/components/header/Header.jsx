7b093de0d8f087244c8248415048a124d6aac456
Ashwin454 <131740882+Ashwin454@users.noreply.github.com>
2025-03-28 10:54:46 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     window.addEventListener("scroll", function () {
         const header = document.querySelector(".header");
         if(this.scrollY >= 80) header.classList.add("scroll-header");
-        else header.classList.remove("scroll-header");
+        else header.classList && header.classList.remove("scroll-header");
     });
    
     const[Toggle, showMenu] = useState(false);
